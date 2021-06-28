eeff7b8685d8f7752b8b887a1195d198b770335d
kazuki.ogata <k-ogata@visor.co.jp>
2021-06-28 18:00:42 +0900

    add: add timeline

---
+++ Instagram/PostData.swift
@@ -0,0 +1,40 @@
+//
+//  PostData.swift
+//  Instagram
+//
+//  Created by KAZUKI-OGATA on 2021/06/25.
+//
+
+import Foundation
+import Firebase
+
+class PostData: NSObject {
+    var id: String
+    var name: String?
+    var caption: String?
+    var date: Date?
+    var likes: [String] = []
+    var isLiked: Bool = false
+
+    init(document: QueryDocumentSnapshot) {
+        self.id = document.documentID
+        
+        let postDic = document.data()
+        self.name = postDic["name"] as? String
+        self.caption = postDic["caption"] as? String
+
+        let timestamp = postDic["date"] as? Timestamp
+        self.date = timestamp?.dateValue()
+
+        if let likes = postDic["likes"] as? [String] {
+            self.likes = likes
+        }
+        if let myId = Auth.auth().currentUser?.uid {
+            // likesの配列の中にmyidが含まれているかチェックすることで、自分がいいねを押しているかを判断
+            if self.likes.firstIndex(of: myId) != nil {
+                // myidがあれば、いいねを押していると認識する。
+                self.isLiked = true
+            }
+        }
+    }
+}
